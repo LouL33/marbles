5c2b9e6bd44f854d183edd76222a2ca0dce018a5
Lou Lee <Lou7532@hotmail.com>
2017-04-25 19:14:37 -0400

    --first commit

---
+++ Marbles/Controllers/MarblesController.cs
@@ -0,0 +1,29 @@
+﻿using Marbles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Marbles.Controllers
+{
+    public class MarblesController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(new { Hello = "world" });
+        }
+
+
+        [HttpPost]
+        public IHttpActionResult AddMarbles(MarblesWithColor marbles)
+        {
+            marbles.Id = new Random().Next(0, 100);
+            marbles.Color = "white";
+            return Ok(marbles);
+        }
+
+    }
+}
---
+++ Marbles/Scripts/JsMarbles.js
@@ -0,0 +1,35 @@
+﻿let ColorClicker = () => {
+    let _fullColor = $("#Color").val();
+    $("#welcomeMessage").html(_fullColor);
+
+};
+
+let addAColor = (data) => {
+    $("#colorList").append($("<li>").html(data.Color));
+}
+
+
+let talkToServer = () => {
+
+    let newMarblesWithColor = {
+        Color: $("#newMarbleColor").val()
+    };
+
+    $.ajax({
+        url: '/api/Marbles',
+        dataType: "json",
+        contenetType: "application/json",
+        //data: JSON.stringify(newMarblesWithColor),
+        type: "POST",
+        success: (data) => {
+            console.log("success", data);
+            addAColor(data);
+        },
+        error: (data) => {
+            console.log("nahhh foo", data);
+        },
+        complete: (data) => {
+            console.log("done", data);
+        }
+    });
+};
